d5bb6898a88aad22e96c36ed7768fdf046cc5da2
Juliet-Olieng <anyangojolieng@gmail.com>
2023-02-02 18:36:45 +0300

    git sample

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main() {
+    var name= "Juliet-Olieng"
+    println(name)
+    var age : Byte = 24
+    println(age)
+var phonenumber : String = "07184992299"
+    println(phonenumber)
+    var weight = 56.5
+    println(weight)
+    var citizenshipkenyan:Boolean =true
+    println(citizenshipkenyan)
+
+
+
+
+}
